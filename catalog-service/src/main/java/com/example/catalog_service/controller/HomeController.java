c4570ea03e17c5f4ada5bef38aa89be78a434462
devscope777 <devscope777@gmail.com>
2026-03-04 13:38:18 -0500

    Add a configuration properties class for getting message

--- catalog-service/src/main/java/com/example/catalog_service/controller/HomeController.java
+++ catalog-service/src/main/java/com/example/catalog_service/controller/HomeController.java
@@ -3,11 +3,20 @@ package com.example.catalog_service.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.catalog_service.config.PolarProperties;
+
 @RestController
 public class HomeController {
+
+    private final PolarProperties polarProperties;
+
+    public HomeController(PolarProperties polarProperties) {
+        this.polarProperties = polarProperties;
+    }
+
     @GetMapping("/")
     public String getGreeting() {
-        return "Welcome to the book catalog!";
+        return polarProperties.greeting();
     }
 
 }
