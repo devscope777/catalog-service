76a6ecd1ed6f1b11e56911394ab77bbcf595b354
devscope777 <devscope777@gmail.com>
2026-03-13 21:00:46 -0400

    Change PolarProperties.java to a class since config refreshes does not work with records

--- src/main/java/com/example/catalog_service/config/PolarProperties.java
+++ src/main/java/com/example/catalog_service/config/PolarProperties.java
@@ -3,5 +3,14 @@ package com.example.catalog_service.config;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "polar")
-public record PolarProperties(String greeting) {
+public class PolarProperties {
+    private String greeting;
+
+    public String greeting() {
+        return greeting;
+    }
+
+    public void setGreeting(String greeting) {
+        this.greeting = greeting;
+    }
 }
