0321afda16255c66e5da568c8d89138b5be03d68
devscope777 <devscope777@gmail.com>
2026-03-25 22:01:56 -0400

    Modify whenPostRequestThenBookCreated test method and add more test methods.

--- src/test/java/com/example/catalog_service/CatalogServiceApplicationTests.java
+++ src/test/java/com/example/catalog_service/CatalogServiceApplicationTests.java
@@ -2,25 +2,86 @@ package com.example.catalog_service;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
 import com.example.catalog_service.domain.Book;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import dasniko.testcontainers.keycloak.KeycloakContainer;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("integration")
-class CatalogServiceApplicationTests {
+@Testcontainers
+public class CatalogServiceApplicationTests {
 	@Autowired
 	private WebTestClient webTestClient;
+
+	private static KeycloakToken bjornTokens;
+	private static KeycloakToken isabelleTokens;
+
+	@Container
+	private static final KeycloakContainer keycloakContainer = new KeycloakContainer("quay.io/keycloak/keycloak:latest")
+			.withRealmImportFile("keycloak_config.json");
+
+	@DynamicPropertySource
+	static void dynamicProperties(DynamicPropertyRegistry registry) {
+		registry.add("spring.security.oauth2.resourceserver.jwt.issuer-uri",
+				() -> keycloakContainer.getAuthServerUrl() + "/realms/PolarBookshop");
+	}
+
+	@BeforeAll
+	static void generateAccessToken() {
+		WebClient webClient = WebClient.builder()
+				.baseUrl(keycloakContainer.getAuthServerUrl() + "/realms/PolarBookshop/protocol/openid-connect/token")
+				.defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+				.build();
+
+		isabelleTokens = authenticatedWith("isabelle", "password", webClient);
+		bjornTokens = authenticatedWith("bjorn", "password", webClient);
+	}
+
+	private static KeycloakToken authenticatedWith(String username, String password, WebClient webClient) {
+		return webClient
+				.post()
+				.body(BodyInserters.fromFormData("grant_type", "password")
+						.with("client_id", "polar-test")
+						.with("username", username)
+						.with("password", password))
+				.retrieve()
+				.bodyToMono(KeycloakToken.class)
+				.block();
+	}
+
+	private record KeycloakToken(String accessToken) {
+		@JsonCreator
+		private KeycloakToken(@JsonProperty("access_token") final String accessToken) {
+			this.accessToken = accessToken;
+		}
+	}
 
 	@Test
 	void whenPostRequestThenBookCreated() {
 		var expectedBook = Book.build("1231231231", "Title", "Author", 9.90, "");
 		webTestClient.post().uri("/books")
+				.headers(headers -> headers.setBearerAuth(isabelleTokens.accessToken))
 				.bodyValue(expectedBook)
 				.exchange()
 				.expectStatus().isCreated()
@@ -28,6 +89,30 @@ class CatalogServiceApplicationTests {
 					assertThat(actualBook).isNotNull();
 					assertThat(actualBook.isbn()).isEqualTo(expectedBook.isbn());
 				});
+	}
+
+	@Test
+	void whenPostRequestUnauthorizedThen403() {
+		var expectedBook = Book.build("1231231231", "Title", "Author", 9.90, "Polarsophia");
+
+		webTestClient.post().uri("/books")
+				.headers(headers -> headers.setBearerAuth(bjornTokens.accessToken()))
+				.bodyValue(expectedBook)
+				.exchange().expectStatus().isForbidden();
+	}
+
+	@Test
+	void whenPostRequestUanuthenticatedThen401() {
+		var expectedBook = Book.build("1231231231", "Title", "Author", 9.90, "Polarsophia");
+
+		webTestClient.post().uri("/books")
+				.bodyValue(expectedBook)
+				.exchange().expectStatus().isUnauthorized();
+	}
+
+	@AfterAll
+	static void cleanup() {
+		keycloakContainer.close();
 	}
 
 }
--- src/test/java/com/example/catalog_service/controller/BookControllerTests.java
+++ src/test/java/com/example/catalog_service/controller/BookControllerTests.java
@@ -3,29 +3,65 @@ package com.example.catalog_service.controller;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.mockito.BDDMockito.given;
 
+import com.example.catalog_service.config.SecurityConfig;
 import com.example.catalog_service.exception.BookNotFoundException;
 import com.example.catalog_service.service.BookService;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt;
 
 @WebMvcTest(BookController.class)
+@Import(SecurityConfig.class)
 public class BookControllerTests {
     @Autowired
-    private MockMvc mvc;
+    private MockMvc mockMvc;
 
     @MockitoBean
     private BookService bookService;
+
+    @MockitoBean
+    private JwtDecoder jwtDecoder;
 
     @Test
     void whenGetBookNotExistingThenShouldReturn404() throws Exception {
         String isbn = "4343345r234532";
         given(bookService.viewBookDetails(isbn)).willThrow(BookNotFoundException.class);
+        mockMvc.perform(get("/books/" + isbn)).andExpect(status().isNotFound());
+    }
 
-        mvc.perform(get("/book/" + isbn)).andExpect(status().isNotFound());
+    @Test
+    void whenDeleteBookWithEmployeeRoleThenShouldReturn204() throws Exception {
+        var isbn = "7373731394";
+        mockMvc
+                .perform(delete("/books/" + isbn)
+                        .with(jwt().authorities(new SimpleGrantedAuthority("ROLE_employee"))))
+                .andExpect(status().isNoContent());
+    }
+
+    @Test
+    void whenDeleteBookWithCustomerRoleThenShouldReturn403() throws Exception {
+        var isbn = "7373731394";
+        mockMvc
+                .perform(delete("/books/" + isbn)
+                        .with(jwt().authorities(new SimpleGrantedAuthority("ROLE_customer"))))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    void whenDeleteBookNotAuthenticatedThenShouldReturn403() throws Exception {
+        var isbn = "7373731394";
+        mockMvc
+                .perform(delete("/books/" + isbn))
+                .andExpect(status().isForbidden());
     }
 
 }
